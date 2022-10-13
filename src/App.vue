0a46c5f1d0c2afcf9e978e370a9582daa2124be5
nedgip <christopher.pigden@gmail.com>
2022-10-13 16:48:09 +1000

    Cleared package-lock and ran npm install

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'EventList' }">Events</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link>
   </nav>
+
   <router-view />
 </template>
 
---
+++ src/views/EventList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="events">
+    <h1>Events for good</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+  name: 'EventList',
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => response.data)
+      .then((data) => (this.events = data))
+      .catch((error) => console.error(error))
+  },
+  components: {
+    EventCard,
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
